2772022c64f9ac8b2c9045394b32fb6fcd1ac103
suvenduweb <150584091+suvenduweb@users.noreply.github.com>
2023-11-17 17:21:40 +0000

    variable done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const acountNumber = 8957564879
+let acountEmail = "ab@gmail.com"
+var acountPassword = 2587413
+acountCity = "Kolkata"
+
+ //acountNumber = 544532121 //not allowed
+
+acountEmail = "sfjkd@gmail.com"
+acountPassword= "655323"
+acountCity = "Hydrabad"
+
+console.log(acountNumber);
+
+
+console.table([acountNumber,acountEmail,acountPassword,acountCity])
