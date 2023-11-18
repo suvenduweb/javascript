aa6c1d7ad3f5524677dbfa71db7a8dbcc4e717eb
suvenduweb <150584091+suvenduweb@users.noreply.github.com>
2023-11-18 17:08:31 +0000

    conversion of data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "hitesh"
+let score = "suvendu"
 
 //console.log(typeof score);
 //console.log(typeof(score));
@@ -12,14 +12,14 @@ let valueInNumber = Number(score)
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = "suvendu"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 //  console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
-// "hitesh" => true
+// "suvendu" => true
 
 let someNumber = 33
 
@@ -41,7 +41,7 @@ let negValue = -value
 // console.log(2%3);
 
 let str1 = "hello"
-let str2 = " hitesh"
+let str2 = " suvendu"
 
 let str3 = str1 + str2
 // console.log(str3);
