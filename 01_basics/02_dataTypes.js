7531e6964049eaf366d0fc21153616aab8ceec58
suvenduweb <150584091+suvenduweb@users.noreply.github.com>
2023-11-18 02:40:41 +0000

    dtatTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treate all js code as newer version
+
+// number 
+// bigint
+// string=> ""
+// boolean=> true/false
+// null=>standalone value
+// undefined=>
+// symbol=> unique
+
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
